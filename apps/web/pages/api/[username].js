55448b9c0a930fcb186c3823af5e63dd2657c5c6
Thomas Davis <thomasalwyndavis@gmail.com>
2023-04-20 21:21:52 +1000

    ASD

--- apps/web/pages/api/[username].js
+++ apps/web/pages/api/[username].js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   console.log(req.body, req.param);
-  res.status(200).json({ name: "John ssss" });
+  res.status(200).json({ name: "John ssss" + req.params.username });
 }
