fbc33acabba81504fec9be211a00790d4ea827c9
Ajax Davis <thomasalwyndavis@gmail.com>
2023-05-15 06:44:38 +1000

    ASd' git

--- apps/registry/pages/api/formatters/coverletter.js
+++ apps/registry/pages/api/formatters/coverletter.js
@@ -1,9 +1,7 @@
 import { ChatGPTAPI } from 'chatgpt';
-// const YAML = require('json-to-pretty-yaml');
 import YAML from 'json-to-pretty-yaml';
-console.log('covereltter');
+
 const format = async function format(resume) {
-  // return JSON.stringify(resume, undefined, 4);
   const OPENAI_API_KEY = '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';
   const data = YAML.stringify(resume);
   const api = new ChatGPTAPI({
@@ -15,6 +13,7 @@ const format = async function format(resume) {
       top_p: 0.8,
     },
   });
+
   const prompt = `
 Hi there, this is my resume in the YAML format. 
   
@@ -22,8 +21,8 @@ ${data}
 
 Please write me a cover letter
 `;
+
   const res = await api.sendMessage(prompt);
-  console.log(res.text);
   return res.text;
 };
 
