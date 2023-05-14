a734ec3339d27e0be36d0bf49dcceb788142dad3
Ajax Davis <thomasalwyndavis@gmail.com>
2023-05-15 05:05:08 +1000

    Asd

--- apps/registry/pages/api/[payload].js
+++ apps/registry/pages/api/[payload].js
@@ -1,75 +1,68 @@
-const sampleResume = require("./samples/resume");
-const fs = require("fs");
-const find = require("lodash/find");
-const axios = require("axios");
+const sampleResume = require('./samples/resume');
+const fs = require('fs');
+const find = require('lodash/find');
+const axios = require('axios');
 
 const FORMATTERS = {
-  qr: require("./formatters/qr"),
-  template: require("./formatters/template"),
-  tex: require("./formatters/tex"),
-  text: require("./formatters/text"),
-  json: require("./formatters/json"),
+  qr: require('./formatters/qr'),
+  template: require('./formatters/template'),
+  tex: require('./formatters/tex'),
+  text: require('./formatters/text'),
+  json: require('./formatters/json'),
 };
 
 // const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const GITHUB_TOKEN = "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"; // @todo - remove this
+const GITHUB_TOKEN = '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'; // @todo - remove this
 
-const FILE_TYPES = new Set(["qr", "json", "tex", "text", "template"]);
+const FILE_TYPES = new Set(['qr', 'json', 'tex', 'text', 'template']);
 
 const failMessage = (message) => {
   return (
     message +
-    ", message @ajaxdavis on twitter if you need help (or tag me in your gist comments @thomasdavis"
+    ', message @ajaxdavis on twitter if you need help (or tag me in your gist comments @thomasdavis'
   );
 };
 
 export default async function handler(req, res) {
   const { theme, payload } = req.query;
-  const { resume } = req.body;
 
-  const payloadSplit = payload.split(".");
+  const payloadSplit = payload.split('.');
 
   const username = payloadSplit[0];
-  let fileType = "template";
+  let fileType = 'template';
 
   if (payloadSplit.length === 2) {
     fileType = payloadSplit[1];
   }
 
   if (!FILE_TYPES.has(fileType)) {
-    return res.status(200).send(failMessage("not supported file type"));
+    return res.status(200).send(failMessage('not supported file type'));
   }
 
   const formatter = FORMATTERS[fileType];
 
   if (!formatter) {
-    return res.status(200).send(failMessage("not supported formatted"));
+    return res.status(200).send(failMessage('not supported formatted'));
   }
 
-  // const rendered = {
-  //   theme,
-  //   username,
-  //   formatted,
-  //   selectedResume,
-  //   fileType,
-  // };
+  const formatted1 = await formatter.format(sampleResume, { theme: 'paper' });
+  return res.status(200).send(formatted1);
 
-  // @todo - short circut dumb common http requests
   if (
     [
-      "favicon.ico",
-      "competition",
-      "stats",
-      "apple-touch-icon.png",
-      "apple-touch-icon-precomposed.png",
-      "robots.txt",
+      'favicon.ico',
+      'competition',
+      'stats',
+      'apple-touch-icon.png',
+      'apple-touch-icon-precomposed.png',
+      'robots.txt',
     ].indexOf(username) !== -1
   ) {
     return res.send(null);
   }
 
   let gistId;
-  console.log("Fetching gistId");
+  console.log('Fetching gistId');
   console.log(`https://api.github.com/users/${username}/gists`);
 
   let gistData = {};
@@ -80,26 +73,26 @@ export default async function handler(req, res) {
       `https://api.github.com/users/${username}/gists`,
       {
         headers: {
-          Authorization: "Bearer " + GITHUB_TOKEN,
+          Authorization: 'Bearer ' + GITHUB_TOKEN,
         },
       }
     );
   } catch (e) {
     console.log(e);
-    return res.send(failMessage("This is not a valid Github username"));
+    return res.send(failMessage('This is not a valid Github username'));
   }
 
   if (!gistData.data) {
-    return res.send(failMessage("This is not a valid Github username"));
+    return res.send(failMessage('This is not a valid Github username'));
   }
 
   const resumeUrl = find(gistData.data, (f) => {
-    return f.files["resume.json"];
+    return f.files['resume.json'];
   });
 
   if (!resumeUrl) {
     return res.send(
-      failMessage("You have no gists named resume.json or your gist is private")
+      failMessage('You have no gists named resume.json or your gist is private')
     );
   }
 
@@ -113,24 +106,24 @@ export default async function handler(req, res) {
       new Date().getTime();
 
     resumeRes = await axios({
-      method: "GET",
-      headers: { "content-type": "application/json" },
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
       url: fullResumeGistUrl,
     });
   } catch (e) {
     // If gist url is invalid, flush the gistid in cache
-    return res.status(200).send(failMessage("Cannot fetch gist, no idea why"));
+    return res.status(200).send(failMessage('Cannot fetch gist, no idea why'));
   }
 
   let realTheme =
-    theme || (resumeRes.data.meta && resumeRes.data.meta.theme) || "flat";
+    theme || (resumeRes.data.meta && resumeRes.data.meta.theme) || 'flat';
 
   realTheme = realTheme.toLowerCase();
 
   const selectedResume = resumeRes?.data ?? sampleResume;
 
   const options = { ...req.query, theme: realTheme };
-  let formatted = "";
+  let formatted = '';
   try {
     formatted = await formatter.format(selectedResume, options);
   } catch (e) {
@@ -139,12 +132,12 @@ export default async function handler(req, res) {
       .status(200)
       .send(
         failMessage(
-          "Cannot format resume, no idea why #likely-a-validation-error"
+          'Cannot format resume, no idea why #likely-a-validation-error'
         )
       );
   }
 
-  res.setHeader("Cache-control", "public, max-age=90");
+  res.setHeader('Cache-control', 'public, max-age=90');
 
   // res.status(200).json(rendered);
   return res.status(200).send(formatted);
