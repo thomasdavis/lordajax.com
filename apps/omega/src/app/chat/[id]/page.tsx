ecc49b30257cc6af955638267f225f3db88f7ff6
Ajax Davis <thomasalwyndavis@gmail.com>
2025-11-20 18:07:37 +1000

    Remove omega app from monorepo

--- apps/omega/src/app/chat/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatLayout } from '@/components/chat/ChatLayout';
-
-export default function ChatPage({ params }: { params: { id: string } }) {
-  return <ChatLayout chatId={params.id} />;
-}
